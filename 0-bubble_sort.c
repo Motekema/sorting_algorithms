486c1a403296ed1d9a9d1a184f5376db5734cc28
Lebohang1821 <millboylebohang@gmail.com>
2023-10-21 12:33:26 -0700

    task-0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -23,7 +23,7 @@ void swap_ints(int *a, int *b)
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t x, lon = size;
+	size_t i, lon = size;
 	bool bubbly = false;
 
 	if (array == NULL || size < 2)
@@ -32,11 +32,11 @@ void bubble_sort(int *array, size_t size)
 	while (bubbly == false)
 	{
 		bubbly = true;
-		for (x = 0; x < lon - x; x++)
+		for (i = 0; i < lon - i; i++)
 		{
-			if (array[x] > array[x + 1])
+			if (array[i] > array[i + 1])
 			{
-				swap_ints(array + x, array + x + 1);
+				swap_ints(array + i, array + i + 1);
 				print_array(array, size);
 				bubbly = false;
 			}
@@ -44,3 +44,4 @@ void bubble_sort(int *array, size_t size)
 		lon--;
 	}
 }
+
