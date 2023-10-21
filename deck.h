def8a6830f7a308da6a91a292e02208387c85596
Lebohang1821 <millboylebohang@gmail.com>
2023-10-21 12:22:34 -0700

    h

---
+++ deck.h
@@ -0,0 +1,50 @@
+#ifndef DECK_H
+#define DECK_H
+
+#include <stdlib.h>
+
+/**
+ * enum kind_e - Enumeration of card suits.
+ * @SPADE: Spades suit.
+ * @HEART: Hearts suit.
+ * @CLUB: Clubs suit.
+ * @DIAMOND: Diamonds suit.
+ */
+typedef enum kind_e
+{
+	SPADE = 0,
+	HEART,
+	CLUB,
+	DIAMOND
+} kind_t;
+
+/**
+ * struct card_s - Plays card
+ *
+ * @value: Value of card
+ * From "Ace" - "King"
+ * @kind: Kind of card
+ */
+typedef struct card_s
+{
+	const char *value;
+	const kind_t kind;
+} card_t;
+
+/**
+ * struct deck_node_s - Deck of card
+ *
+ * @card: Pointer - card of node
+ * @prev: Pointer - previous node of list
+ * @next: Pointer - next node of list
+ */
+typedef struct deck_node_s
+{
+	const card_t *card;
+	struct deck_node_s *prev;
+	struct deck_node_s *next;
+} deck_node_t;
+
+void sort_deck(deck_node_t **deck);
+
+#endif /* DECK_H */
